79f7fe664b09251680de8b205bb5fe029b038cfb
AnkitC1598 <ankitc@letsupgrade.in>
2022-09-20 18:38:03 +0530

    :sparkles: added UserFollowed icon

--- icons/index.jsx
+++ icons/index.jsx
@@ -4,3 +4,4 @@ export { default as Instagram } from "./Instagram";
 export { default as Linkedin } from "./Linkedin";
 export { default as Medal } from "./Medal";
 export { default as Twitter } from "./Twitter";
+export { default as UserFollowed } from "./UserFollowed";
