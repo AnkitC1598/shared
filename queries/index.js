ac36eb46b3ac06b682b530983d1922e6337680b5
AnkitC1598 <ankitc@letsupgrade.in>
2022-09-27 14:04:14 +0530

    :hammer: default profile & cover image for user

--- queries/index.js
+++ queries/index.js
@@ -2,6 +2,16 @@ import { fetchWithToken } from "../services/axios";
 
 export const fetchUser = async ({ queryKey }) => {
 	const [_, username] = queryKey;
-	const resp = await fetchWithToken.get(`/v1/users/${username}`);
-	return resp.data.results.data;
+	let resp = await fetchWithToken.get(`/v1/users/${username}`);
+	resp = resp.data.results.data;
+
+	if (resp.profileImage === null)
+		resp.profileImage = `https://avatars.dicebear.com/api/initials/${resp.username.replace(
+			" ",
+			"-"
+		)}.svg`;
+	if (resp.coverImage === null)
+		resp.coverImage = "https://source.unsplash.com/1600x900/?technology";
+
+	return resp;
 };
