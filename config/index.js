caf7f559403ceb581591c7c7b80d46c7615717d9
AnkitC1598 <ankitc@letsupgrade.in>
2022-09-30 11:27:20 +0530

    :sparkles: loadingToastConfig added

---
+++ config/index.js
@@ -0,0 +1,1 @@
+export { default as loadingToastConfig } from "./loadingToastConfig";
---
+++ config/loadingToastConfig.js
@@ -0,0 +1,8 @@
+export default {
+	isLoading: false,
+	closeOnClick: true,
+	pauseOnFocusLoss: false,
+	pauseOnHover: false,
+	autoClose: 1000,
+	draggable: true,
+};
